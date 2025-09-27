4a5e2ba5b0038292b0db37f0215f560f2bf4da88
Abrie PC <www.headoff.com@gmail.com>
2025-09-27 13:22:29 +0200

    Change standard ports

--- obj/Debug/net9.0/MyBoardGameList.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyBoardGameList.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBoardGameList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f43a8d916419294ea342395893e4044cb19e3101")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBoardGameList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBoardGameList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
