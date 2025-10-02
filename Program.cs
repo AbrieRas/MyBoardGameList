bf3d05d00d74eca8b908f585523f03f640065b4d
Abrie PC <www.headoff.com@gmail.com>
2025-10-02 06:24:36 +0200

    Add Cors and Cache

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,24 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// CORS
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(cfg =>
+    {
+        cfg.WithOrigins(builder.Configuration["AllowedOrigins"]);
+        cfg.AllowAnyHeader();
+        cfg.AllowAnyMethod();
+    });
+    options.AddPolicy(name: "AnyOrigin",
+        cfg =>
+        {
+            cfg.AllowAnyOrigin();
+            cfg.AllowAnyHeader();
+            cfg.AllowAnyMethod();
+        });
+});
+
 var app = builder.Build();
 
 app.UsePathBase(builder.Configuration["/BoardGames"]);
@@ -15,14 +35,8 @@ app.UsePathBase(builder.Configuration["/BoardGames"]);
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    //app.MapOpenApi();
     app.UseDeveloperExceptionPage();
 }
-
-//if (app.Environment.IsStaging())
-//{
-//    app.MapOpenApi();
-//}
 
 if (app.Configuration.GetValue<bool>("UseOpenApi"))
 {
@@ -32,17 +46,28 @@ if (app.Configuration.GetValue<bool>("UseOpenApi"))
 if (app.Configuration.GetValue<bool>("UseDeveloperExceptionPage"))
     app.UseDeveloperExceptionPage();
 else
-    app.UseExceptionHandler("/test");
+    app.UseExceptionHandler("/error");
 
 app.UseHttpsRedirection();
+
+// Use CORS
+app.UseCors();
 
 app.UseAuthorization();
 
 // Minimal API
-app.MapGet("/test", () => Results.Problem());
-app.MapGet("/error/test", () => { throw new Exception("test"); });
+app.MapGet("/error",
+    [EnableCors("AnyOrigin")]
+    [ResponseCache(NoStore = true)] () =>
+    Results.Problem());
+
+app.MapGet("/error/test",
+    [EnableCors("AnyOrigin")]
+    [ResponseCache(NoStore = true)] () =>
+    { throw new Exception("test"); });
 
 // Controllers
-app.MapControllers();
+app.MapControllers()
+    .RequireCors("AnyOrigin");
 
 app.Run();
--- obj/Debug/net9.0/MyBoardGameList.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyBoardGameList.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBoardGameList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+831d2582f5a429c399d3ccb3350a1486cae853db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcd4511ed993ba53c4b729a369ffe882c1175e4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBoardGameList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBoardGameList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
